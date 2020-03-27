5e27df8711ce88f019bca1393722717cb2eea7ba
pratyush-3000 <59516417+pratyush-3000@users.noreply.github.com>
2020-03-26 21:47:12 -0700

    Bugs fixed in project file

--- ProjectFile.sql
+++ ProjectFile.sql
@@ -9,20 +9,21 @@ JOIN TblDept dt on
  --2.Print the name of the students with the lowest Gpa. 
  SELECT S1.Sname as StudentName, S1.GPA as GPA
  FROM TblStudent S1
- WHERE S1.GPA IN (
+ WHERE S1.GPA = (
 					 SELECT MIN(s.gpa)
 					 FROM TblStudent s
 					)
  -------------------------------------------------------------------------------
  --3.For each Computer Science class, print the class number, section number, and the average gpa of the students enrolled in the class.
- SELECT S.SID, AVG(GPA), M.Dname as DepartmentName, se.Cno as ClassName, se.Sectno as SectionName 
+ SELECT AVG(GPA), M.Dname as DepartmentName, se.Cno as ClassName, se.Sectno as SectionName 
  FROM TblMajor M 
  join TblStudent s
   on m.Sid = s.Sid
   JOIN TblSection se
    on m.dname = se.dname
-   GROUP BY s.sid, m.dname, se.Cno, se.Sectno 
- HAVING m.dname = 'Computer Sciences'
+   WHERE m.dname = 'Computer Sciences'
+   GROUP BY  m.dname, se.Cno, se.Sectno 
+ 
  ---------------------------------------------------------------------------
  --4. Print the names and section numbers of all classes with more than six students enrolled in them. 
  Select se.dname as ClassName,se.Cno as ClassNumber, se.Sectno as SectionNumber
@@ -35,20 +36,26 @@ JOIN TblDept dt on
 				)
 ----------------------------------------------------------------------------------
 --5. Print the names and sids of the students enrolled in the most classes.
-SELECT e.sid ,count(e.cno) counts ,s.sname
-FROM TblEnroll e
-JOIN TblStudent s on e.sid = s.sid
-Group by e.sid,s.Sname
-HAVING count(e.cno)> 1
-order by e.sid
+SELECT MAX(mycount) counts, s.sname,S.sid
+FROM (
+           SELECT E.SID, COUNT(CNO) mycount 
+		   FROM TblEnroll e		   
+		    Group by e.sid
+	 ) E
+			JOIN TblStudent s on e.sid = s.sid
+			Group by S.Sname ,S.Sid
 ---------------------------------------------------------------------------------------
 --6. Print the names of students that have one or more majors who are between 18 and 20 years old. 
-SELECT e.sid ,count(e.cno) counts ,s.sname,s.age 
-FROM TblEnroll e
-JOIN TblStudent s on e.sid = s.sid
-Group by e.sid,s.Sname,s.age 
-HAVING count(e.Cno)>= 1 and s.age between 18 and 20 
-order by e.sid
+SELECT s.sname,s.age 
+FROM TblStudent s
+JOIN
+(		SELECT count(*) as s_count, m.sid sid
+		FROM TblMajor m 
+		GROUP by m.sid
+		HAVING count(*) >= 1
+) Sub  on sub.sid = s.sid
+where s.age between 18 and 20 
+
 --------------------------------------------------------------------------------------------
 --7. Print the names and majors of students who are taking one of the College Geomerty courses. 
 SELECT s.sname, d.dname, c.cname 
@@ -72,15 +79,17 @@ FROM TblCourse c
 JOIN TblDept d on d.dname = c.dname
 Join TblEnroll e on e.Cno = c.cno
 JOIN TblStudent s on s.sid = e.Sid
-WHERE c.Cname like '%Geometry%' or c.dname like '%Computer%'
+WHERE c.dname like '%Geometry%' AND c.dname like '%Computer%'
+
 --------------------------------------------------------------------------------
 --10. Print the age difference between the oldest and the youngest Computer Sciences Major.
 SELECT c.dname,(MAX(s.age)- MIN(s.age)) as difference
 FROM TblCourse c
 Join TblEnroll e on e.Cno = c.cno
 JOIN TblStudent s on s.sid = e.Sid
+WHERE c.dname like'%Computer%'
 GROUP BY  c.dname
-HAVING c.dname like'%Computer%'
+
 ---------------------------------------------------------------------------------------
 --11. For each department that has one or more majors with a GPA under 1.0,
 --print the name of the department and the average GPA of its majors. 
@@ -89,7 +98,7 @@ FROM TblEnroll e
 GROUP BY e.dname 
 HAVING  e.Dname in (
 					SELECT d.Dname
-					FROM TblDept d --JOIN  TblCourse c on d.Dname = c.Dname
+					FROM TblDept d 
 					JOIN TblEnroll e on e.Dname = d.dname
 					WHERE e.Grade < 1.0
 					)
